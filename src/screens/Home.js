83a12a7654b1fda9f41d140bbf5b558e92ca213e
Basant Soni <basantech89@gmail.com>
2019-11-24 20:20:50 +0530

    Update Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,12 +10,16 @@ const structure = {
     file1: { content: 'The is the content for file1 in the <public> directory.' },
     file2: { content: 'The is the content for file2 in the <public> directory.' },
     file3: { content: 'The is the content for file3 in the <public> directory.' },
+    projects: {
+      'Astroids': { content: 'Content for this file' },
+    },
+    '.hiddenFile': { content: 'its a hidden file' }
   },
   'README.md': { content: 'Hello There' },
 };
 
 const Home = (props) => {
-  const { primary, opacity } = props;
+  const { primary } = props;
   const chars = ['h', 'a', 'c', 'k', '0', '~', '!', '#', '$'];
   const getRandomChar = () => chars[Math.floor(Math.random() * chars.length)];
   const [char, setChar] = useState(getRandomChar());
@@ -44,8 +48,12 @@ const Home = (props) => {
   };
 
   return (
-    <div className="container">
-      <Terminal history={history} structure={structure} extensions={extensions} theme={Terminal.Themes.DARK} prefix="Basant@HOME" />
+    <div className="container" style={{ fontSize: 20, lineHeight: 1.9 }}>
+      <Terminal history={history}
+      structure={structure} 
+      extensions={extensions} 
+      theme={Terminal.Themes.DARK} 
+      prefix="Basant@HOME" />
     </div>
   );
 };
